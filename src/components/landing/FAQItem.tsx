803cae5d7f681ab3767f930bbe584a6c2624c4ba
Ozymandias-9 <galac.9173@gmail.com>
2024-01-10 19:18:19 -0600

    fix: fix faq section

--- src/components/landing/FAQItem.tsx
+++ src/components/landing/FAQItem.tsx
@@ -9,8 +9,8 @@ interface Props {
 export default function FAQItem({ question, answer }: Props) {
   const [answerIsOpen, setAnswerIsOpen] = useState(false);
   return (
-    <div onClick={() => setAnswerIsOpen(!answerIsOpen)} className={`cursor-pointer flex flex-col py-6 ${answerIsOpen ? 'space-y-4' : ''}`}>
-      <div className='flex justify-between items-center'>
+    <div className={`cursor-pointer flex flex-col py-6 ${answerIsOpen ? 'space-y-4' : ''}`}>
+      <div onClick={() => setAnswerIsOpen(!answerIsOpen)}  className='flex justify-between items-center gap-3'>
         <h3 className="font-semibold text-xl">{question}</h3>
         <button className={`duration-300 animation-ping transition-all outline-none focus:outline-none ${answerIsOpen ? '-rotate-180' : 'rotate-0'}`}>
           <Icon icon="fa6-solid:chevron-down"/>
