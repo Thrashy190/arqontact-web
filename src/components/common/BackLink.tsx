ab44761101528415b799d4b40ddc813db6936cc5
Ozymandias-9 <galac.9173@gmail.com>
2024-02-01 21:47:32 -0600

    feat: merging my-profile/options to configuration

--- src/components/common/BackLink.tsx
+++ src/components/common/BackLink.tsx
@@ -1,7 +1,7 @@
 import { Icon } from "@iconify/react";
 
 export default function BackLink() {
-    return <div onClick={() => window.history.back()} className='z-[6] text-xl'>
+    return <div onClick={() => window.history.back()} className='cursor-pointer z-[6] text-xl'>
         <Icon icon="fa-solid:chevron-left" className="w-5 h-5"/>
     </div>
 }
